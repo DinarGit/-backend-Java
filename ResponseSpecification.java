620785b69023bb08ba5b3ae00cb955615fe21c10
Dinar <52482979+DinarGit@users.noreply.github.com>
2022-09-06 12:27:21 +0500

    Update ResponseSpecification.java

--- ResponseSpecification.java
+++ ResponseSpecification.java
@@ -79,3 +79,88 @@ public class ResponseSpecification {
 
 
 
+package com.geekbrains.builder;
+
+import org.junit.jupiter.api.Test;
+
+public class User {
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final String email;
+
+    private User(Builder builder) {
+        name = builder.name;
+        surname = builder.surname;
+        address = builder.address;
+        email = builder.email;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder{
+        private String name;
+        private String surname;
+        private String address;
+        private String email;
+
+        public Builder setName(String surname) {
+            this.name = name;
+        }
+}
+    @Test
+    void getRecipePositiveTest() {
+        given()
+                .when()
+                .get("https://api.spoonacular.com/recipes/716429/information")
+                .then()
+                .spec(responseSpecification);
+    }
+
+
+    <?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>com.geekbrains.test.aug2022</groupId>
+    <artifactId>backend-test</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <dependencies>
+        <dependency>
+            <groupId>org.junit.jupiter</groupId>
+            <artifactId>junit-jupiter</artifactId>
+            <version>RELEASE</version>
+            <scope>compile</scope>
+        </dependency>
+        <dependency>
+        <groupId>com.fasterxml.jackson.core</groupId>
+        <artifactId>jackson-core</artifactId>
+        <version>${jackson.version}</version>
+    </dependency>
+    <dependency>
+        <groupId>com.fasterxml.jackson.core</groupId>
+        <artifactId>jackson-annotations</artifactId>
+        <version>${jackson.version}</version>
+    </dependency>
+    <dependency>
+        <groupId>com.fasterxml.jackson.core</groupId>
+        <artifactId>jackson-databind</artifactId>
+        <version>${jackson.version}</version>
+    </dependency>
+        <dependency>
+            <groupId>org.projectlombok</groupId>
+            <artifactId>lombok</artifactId>
+            <version>${lombok.version}</version>
+        </dependency>
+    </dependencies>
+    <properties>
+        <maven.compiler.source>11</maven.compiler.source>
+        <maven.compiler.target>11</maven.compiler.target>
+        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+    </properties>
+
+</project>
